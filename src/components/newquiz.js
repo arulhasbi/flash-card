dbdca774255851f2e57d1c683b5eb452fcd49b47
fakhrul hasbi <fakhrulhasbi@fakhruls-MacBook-Pro.local>
2022-11-10 12:51:20 +0800

    added formik to handle cards array form

--- src/components/newquiz.js
+++ src/components/newquiz.js
@@ -1,81 +1,136 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAllTopics } from "../features/topics/topicsSlice";
 import { loadTopics } from "../features/topics/topicsSlice";
 
-export const NewQuiz = () => {
-  const [quizTitle, setQuizTitle] = useState("");
-  const [selectedTopic, setSelectedTopic] = useState("");
+import { Formik, Field, Form, FieldArray } from "formik";
 
+export const NewQuiz = () => {
   const dispatch = useDispatch();
   const allTopics = useSelector(selectAllTopics);
-
   useEffect(() => {
     dispatch(loadTopics());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const handleAdd = () => {};
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleTopicChange = (topicID, callback) => {
+    if (allTopics.length !== 0) {
+      if (topicID === "") {
+        callback("quiz.topic.id", "");
+        callback("quiz.topic.name", "");
+        callback("quiz.topic.iconID", "");
+      } else {
+        const topic = allTopics.find((topic) => topic.id === topicID);
+        callback("quiz.topic.id", topic.id);
+        callback("quiz.topic.name", topic.name);
+        callback("quiz.topic.iconID", topic.iconID);
+      }
+    }
   };
-
   return (
     <NewQuizWrapper className="mt-10">
       <NewQuizMaxWidth>
-        <section onSubmit={handleSubmit}>
-          <p className="font-bold text-2xl text-center grow">
-            Create a new quiz
-          </p>
-          <form>
-            <div className="form-control mt-5">
-              <input
-                required
-                type="text"
-                name="quiz-title"
+        <Formik
+          initialValues={{
+            quiz: {
+              title: "",
+              topic: {
+                id: "",
+                name: "",
+                iconID: "",
+              },
+            },
+            cards: [],
+          }}
+          onSubmit={(values) => {
+            window.alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({ setFieldValue, values }) => (
+            <Form>
+              <p className="font-bold text-2xl text-center">
+                Create a New Quiz
+              </p>
+              <Field
+                name="quiz.title"
                 placeholder="Quiz Title"
-                className="w-full py-2 px-3 rounded-lg border-2 border-cyan-600"
-                value={quizTitle}
-                onChange={({ target }) => setQuizTitle(target.value)}
+                className="w-full py-2 px-3 rounded-lg border-2 border-cyan-600 mt-5"
               />
-            </div>
-            <div className="form-control mt-5">
-              <select
-                required
-                name="topic"
-                id="topic"
-                className="w-full py-2.5 px-3 rounded-lg border-2 border-cyan-600"
-                value={selectedTopic}
-                onChange={({ target }) => setSelectedTopic(target.value)}
-              >
-                <option value="">Choose a topic</option>
-                {allTopics.length !== 0 &&
-                  allTopics.map((topic) => (
-                    <option key={topic.id} value={topic.id}>
-                      {topic.name}
-                    </option>
-                  ))}
-              </select>
-            </div>
-            <div className="flex justify-between">
-              <button
-                type="button"
-                className="font-bold border mt-5 py-2 px-6 button-15"
-                onClick={handleAdd}
-              >
-                Add a card
-              </button>
-              <button
-                type="submit"
-                className="font-bold border mt-5 py-2 px-6 button-15"
-              >
-                Create Quiz
-              </button>
-            </div>
-          </form>
-        </section>
+              <Field name="quiz.topic.id">
+                {({ field }) => (
+                  <select
+                    {...field}
+                    className="w-full py-2.5 px-3 rounded-lg border-2 border-cyan-600 mt-5"
+                    onChange={({ target }) =>
+                      handleTopicChange(target.value, setFieldValue)
+                    }
+                  >
+                    <option value="">Choose a topic</option>
+                    {allTopics.length !== 0 &&
+                      allTopics.map((topic) => (
+                        <option key={topic.id} value={topic.id}>
+                          {topic.name}
+                        </option>
+                      ))}
+                  </select>
+                )}
+              </Field>
+              <hr className="mt-5" />
+              <FieldArray
+                name="cards"
+                render={(arrayHelpers) => {
+                  return (
+                    <div>
+                      <div className="max-h-[300px] overflow-auto">
+                        {values.cards.length > 0 &&
+                          values.cards.map((_, index) => (
+                            <div key={index}>
+                              <Field
+                                name={`cards.${index}.front`}
+                                className="w-full py-2 px-3 rounded-lg border-2 border-cyan-600 mt-5"
+                                placeholder="Front"
+                              />
+                              <Field
+                                name={`cards.${index}.back`}
+                                className="w-full py-2 px-3 rounded-lg border-2 border-cyan-600 mt-5"
+                                placeholder="Back"
+                              />
+                              <div className="text-right mt-2">
+                                <button
+                                  type="button"
+                                  className="text-red-600 font-bold"
+                                  onClick={() => arrayHelpers.remove(index)}
+                                >
+                                  Remove Card
+                                </button>
+                              </div>
+                            </div>
+                          ))}
+                      </div>
+                      <div className="flex justify-between">
+                        <button
+                          type="button"
+                          className="font-bold border mt-5 py-2 px-6 button-15"
+                          onClick={() =>
+                            arrayHelpers.push({ front: "", back: "" })
+                          }
+                        >
+                          Add Card
+                        </button>
+                        <button
+                          type="submit"
+                          className="font-bold border mt-5 py-2 px-6 button-15"
+                        >
+                          Create Quiz
+                        </button>
+                      </div>
+                    </div>
+                  );
+                }}
+              />
+            </Form>
+          )}
+        </Formik>
       </NewQuizMaxWidth>
     </NewQuizWrapper>
   );
