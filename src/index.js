edcc6ae4794e2d4e4a27005e5e66be0520cb1422
fakhrul hasbi <fakhrulhasbi@fakhruls-MacBook-Pro.local>
2022-11-10 22:19:22 +0800

    remove development only msw

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start({
-    onUnhandledRequest: "bypass",
-  });
-}
+const { worker } = require("./mocks/browser");
+worker.start({
+  onUnhandledRequest: "bypass",
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
